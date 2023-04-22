7fb95f528411068fde26f9fc8231ee9df0fe7d13
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-04-23 01:08:21 +0530

    Hook-project initialization and description of need of it in App.js.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        {/* ************ NEED OF HOOKS ************
+
+        Reason Set 1 :
+          *  `this` keyword works differently in JS. So, understand how it works.
+          *  Remember to bind event handlers in class component
+          *  Classes don't minify very well & make hot reloading very unreliable
+           
+        
+        Reason Set 2 :
+          *  There is no particular way to Reuse stateful component logic
+          *  HOC & render prop method, do solve/address this problem.
+          *   (but makes the code hard to follow)
+          * 
+          *   So, that's why there is a need to share a needful Logic in better way.
+          * 
+      
+
+        Reason Set 3 :
+          *  Related code is not organised at one place.
+                  eg. : create component for complex scenarios such as [Data fetching and subscribing to events]
+
+          *  Ex : Data fetching : in componentDidMount & componentDidUpdate
+          *  Ex : Event listeners : in componentDidMount & componentDidUnmount
+        *  { means both the above examples are implemented in "componentDidMount", which completely different from each other.} 
+          *
+          * 
+          * 
+          * 
+          * Because of stateful - Cannot break component into smaller ones
+            
+
+
+        */}
+
+
+
     </div>
   );
 }
