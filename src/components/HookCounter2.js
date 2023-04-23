1996d13fe2b249e879b8dd1d103283d8e5649f66
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-04-24 00:02:00 +0530

    Here we are seeing the issue while using 'setState' and calling a function for Increment5 (we've use for loop to increment it by 1, 5 times). which is not working correctly and increasing it by 1

--- src/components/HookCounter2.js
+++ src/components/HookCounter2.js
@@ -7,7 +7,18 @@ function HookCounter2() {
 
     const [count , setCount] = useState(initialVal)
 
+    const incrementFive = () => {
+        for (let i = 0; i<5; i++)
+        {
+            setCount(count + 1)
+        }
+    }
 
+    const decrementFive = () => {
+         
+            setCount(count - 5)
+         
+    }
 
     return (
     <div>
@@ -16,6 +27,10 @@ function HookCounter2() {
         <button onClick={() => setCount(initialVal)}>Reset</button>
         <button onClick={() => setCount(count + 1)}>Incement</button> <br></br>
 
+        {/* eg. for vulnerability */}
+
+        <button onClick={incrementFive}>Increment 5 </button> : 'using for loop, <b> not working </b> properly'<br/>
+        <button onClick={decrementFive}>Decrement 5 </button> : '<b> without </b> using for loop. <b> direct</b> increment using <b> "setState"</b>  '
         
 
     </div>
