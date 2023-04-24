2c6fd517eafec4089bde082b189b3486f4f855be
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-04-24 20:29:01 +0530

    `

--- src/components/HookCounter2.js
+++ src/components/HookCounter2.js
@@ -10,26 +10,32 @@ function HookCounter2() {
     const incrementFive = () => {
         for (let i = 0; i<5; i++)
         {
-            setCount(count + 1)
-        }
+{/*  #1   */}             setCount(prevVal => prevVal + 1);         // #1  
+                   // functionName(previous-value => previous-value & other do-able operations)
+        } 
     }
 
     const decrementFive = () => {
          
-            setCount(count - 5)
-         
+            setCount(prevValue => prevValue - 5);              
     }
 
     return (
     <div>
         Count : {count} <br></br>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
         <button onClick={() => setCount(initialVal)}>Reset</button>
-        <button onClick={() => setCount(count + 1)}>Incement</button> <br></br>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>Incement</button> <br></br>
 
-        {/* eg. for vulnerability */}
+        {/* eg. for vulnerability 
 
-        <button onClick={incrementFive}>Increment 5 </button> : 'using for loop, <b> not working </b> properly'<br/>
+        to tackel which, we will be using 2nd form of 'setCount' func.
+            We will be passing : instead of passing the new-State-variable.
+#1                                    we'll pass a 'function' that has access to old-state-value.    
+        
+        */}
+
+        <button onClick={incrementFive}>Increment 5 </button> : 'using for loop, <b> Now it's working </b> properly'<br/>
         <button onClick={decrementFive}>Decrement 5 </button> : '<b> without </b> using for loop. <b> direct</b> increment using <b> "setState"</b>  '
         
 
