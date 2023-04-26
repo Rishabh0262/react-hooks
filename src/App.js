701f81371dd5ae64b400010dc1f5026c6aa9594b
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-04-26 13:14:48 +0530

    We get to understand how do we use Arrays with useState, where we have added the item in the form of object in the array.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
 
 function App() {
   return (
     <div className="App">
 
+
+      {/* ***** useState with Array,  Hook in React *********** */}
+      <HookCounter4 />
+
+
+
+
+
+
       {/* ***** useState with object,  Hook in React *********** */}
-      <HookCounter3 />
+      {/* <HookCounter3 /> */}
 
 
 
