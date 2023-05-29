378b591585cca7eeb53d989685bea718880d680d
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-05-30 00:01:39 +0530

    Here we have 'almost' replicated RCE ClassTimer.js in HookTimer.js. Except Clear-Hook-Timer <Button> is not working. We need to use
    'useRef' to make it work.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import FocusInput from './components/FocusInput';
 import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       
       {/*     useRef-Hook     */}
       <ClassTimer />
-
+      <HookTimer />
 
 
 
