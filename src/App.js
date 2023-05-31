610f2cc8c16ee4590086086caf6283040112b447
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-05-31 15:00:03 +0530

    ******** We created 2 Counter-components having same functionalities in both the components. Such as Increment/Decrement/Reset.
    That we can easily use a Custom-Hook

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import DocTitle1 from './components/DocTitle1';
 import DocTitle2 from './components/DocTitle2';
+import Counter1 from './components/Counter1';
+import Counter2 from './components/Counter2';
 
 function App() {
   return (
     <div className="App">
 {/*   ******* basic counter app using useState/useEffect to change the Doc-Title ********   */}
-          <DocTitle1 />
+
+          {/* <DocTitle1 /> */}
+
+
 
 {/*    We want to change Doc-Title from the other component.
        We basically reused the entire <code/>. Which we should avoid.
 */}
-        <DocTitle2 />
+        {/* <DocTitle2 /> */}
 
 
 
 {/*    To avoid Coping the Code. We can create a Custom-Hook.
       That's called the useDocumentTitle  Hook
 */}
+
+
+
+
+
+
+
+{/*     Here we are trying to make an another attempt to use Custom-Hook
+
+*/}
+      <Counter1 />
+      <Counter2 />
+
+
 
     </div>
   );
